7374ff362af2dc98dc0a56a4d7c9133d01c07f8b
KobeNorrise <Kobewu522@163.com>
2020-06-20 21:41:59 +0100

    Update plugin info and details

---
+++ helloworld/helloworld.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Plugin Name: Helloworld
+Plugin URI: https://github.com/KobeNorris/WordPress-Plugin
+Description: Insert a block contains hello world to the page
+Version: 1.0
+Author: KobeNorris
+Author URI: https://github.com/KobeNorris
+License: GPLv2
+
+Copyright 2020 Kobe Norris (email : KobeNorrisWu@gmail.com)
+
+This program is free software; you can redistribute it and/or modify it under the terms of the GNU
+General Public License version 2, as published by the Free Software Foundation. You may NOT assume
+that you can use any other version of the GPL.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*/
+
+
+
+?>
